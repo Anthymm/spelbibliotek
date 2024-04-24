0480c72ea207d0d80e080458b1d913449c9c1546
Victor <victor.ande@icloud.com>
2024-04-24 13:28:17 +0200

    Fungerande Template

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const cors = require('cors');
+const connectionMySQL = require("./connectionMySQL");
 
+// Import express and cors
+const express = require("express");
+const cors = require("cors");
+
+//
 const app = express();
 const port = 3000;
 
@@ -11,12 +15,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-app.use(express.static('public'));
 
-const bookRoutes = require('./routes/bookRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
+// Serve static files in webbserver
+app.use(express.static("public"));
 
+// Import routes
+const bookRoutes = require("./routes/bookRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+// Create route
+app.get("/users", async (req, res) => {
+  let sql = "SELECT * FROM users";
+  try {
+    await connectionMySQL.query(sql, (error, results, fields) => {
+      if (error) {
+        if (error) throw error;
+      }
+      res.json(results);
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+// Use routes in express app
 app.use(bookRoutes);
 app.use(categoryRoutes);
 
+// Start express webb
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ frontend/src/views/UsersView.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    getUsers() {
+      fetch("http://localhost:3000/users")
+        .then((response) => response.json())
+        .then((data) => (this.users = data));
+    },
+  },
+  
+  created() {
+    this.getUsers();
+  },
+};
+</script>
+
+<template>
+  <main>
+    <div v-for="user in users">
+      {{ user.name }}
+      {{ user.address }}
+    </div>
+  </main>
+</template>
